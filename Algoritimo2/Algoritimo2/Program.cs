c73687e1ce925f921bbb86f779313f7cf38ca6e5
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-03-25 21:50:29 -0300

    LEITURA DE VARIAVEIS

--- Algoritimo2/Algoritimo2/Program.cs
+++ Algoritimo2/Algoritimo2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int idade = 10;
+string nomeCliente = "Kaina Brum Habekost";
+decimal  preco = 20M;
+char letra = 'a';
+var nomecompleto = "Kaina Brum Habekost";
+var altura = 167;
+
+Console.WriteLine("Digite seu nome:");
+nomecompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo" + nomecompleto);
+Console.WriteLine();
+Console.Write('Digite sua idade:');
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada" + idade);
+if (idade < 18)
+    Console.WriteLine("Voce não pode estar aqui !");
+
+Console.ReadKey();
